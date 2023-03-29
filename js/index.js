c4615c56eaaabaa9809014caf9f9d455eb887e4a
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-03-29 19:58:18 +0300

    123

--- js/index.js
+++ js/index.js
@@ -1,55 +1,37 @@
-gsap.from(".hero__title", {
-    y: 100,
-    opacity: 0,
-    duration: 1
-})
-
-gsap.from(".hero__descr", {
-    opacity: 0,
-    duration: 1,
-    delay: 1,
-})
-
-gsap.from(".hero__btn", {
-    y: 100,
-    opacity: 0,
-    duration: 1
-})
-
-gsap.from(".photos-wrap-img-1", {
-    opacity: 0,
-    duration: 1,
-    delay: 1,
-})
-
-gsap.from(".photos-wrap-img-2", {
-    opacity: 0,
-    duration: 1,
-    delay: 1.3,
-})
-
-gsap.from(".photos-wrap-img-3", {
-    opacity: 0,
-    duration: 1,
-    delay: 1.7,
-})
-
-gsap.from(".photos__author", {
-    opacity: 0,
-    duration: 1,
-    delay: 2,
-});
-
-let tl = gsap.timeline({ paused: true })
-tl.fromTo(".menu", { display: "none", y: 100, opacity: 0, }, { display: "block", y: 0, opacity: 1, duration: 2, ease: "circle" }, '-=0.5');
-tl.from(".menu__top", { delay: 0.5, opacity: 0, y: -50, duration: 0.5, ease: "circle" },
-    '-=0.5');
-tl.from(".nav__list", { delay: 0.4, opacity: 0, y: 25, duration: 0.5, ease: "circle" },
-    '-=0.5')
-tl.from(".social, .menu__right, .sub-menu", { delay: 0.7, y: 25, opacity: 0, duration: 0.5, ease: "circle" },
-    '-=0.5');
-
-let open = document.querySelector('.burger')
-let close = document.querySelector('.close')
-open.addEventListener("click", () => tl.play())
-close.addEventListener("click", () => tl.reverse());
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    centeredSlides: 1,
+    initialSlide: 1,
+    slidesPerView: 'auto',
+    spaceBetween: 50,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 0,
+        modifier: 200,
+        slideShadows : true,
+      },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+        576: {
+          spaceBetween: 10,
+        },
+    }
+  });
