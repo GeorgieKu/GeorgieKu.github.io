4b5b2045036223d4b3a1a9736a0bda59344cc317
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-04-24 16:53:31 +0300

    123

--- js/index.js
+++ js/index.js
@@ -11,12 +11,31 @@ const swiper = new Swiper('.promotion__swiper', {
 
 const swiper2 = new Swiper('.specialist__swiper', {
     slidesPerView: 5,
-    loop: true,
+    loop: false,
     spaceBetween: 100,
     navigation: {
         nextEl: '.specialist-button-next',
         prevEl: '.specialist-button-prev',
     },
+    breakpoints: {
+        // when window width is >= 320px
+        1600: {
+            slidesPerView: 5,
+            spaceBetween: 80
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 80
+        },
+        375: {
+            slidesPerView: 'auto',
+            spaceBetween: 30
+        }
+    }
     
 });
 
@@ -34,7 +53,7 @@ const swiper3 = new Swiper('.services__swiper', {
           slidesPerView: 3,
           spaceBetween: 80
         },
-        1240: {
+        375: {
             slidesPerView: 'auto',
             spaceBetween: 30
         }
@@ -51,5 +70,16 @@ const swiper4 = new Swiper('.review__swiper', {
         nextEl: '.review-button-next',
         prevEl: '.review-button-prev',
     },
+    breakpoints: {
+        // when window width is >= 320px
+        2000: {
+          slidesPerView: 3,
+          spaceBetween: 80
+        },
+        375: {
+            slidesPerView: 'auto',
+            spaceBetween: 20
+        }
+    }
     
 });
