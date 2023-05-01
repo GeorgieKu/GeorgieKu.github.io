9cbc307bd6c52bf3ec4c9896c25a8e94266bb030
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-05-01 20:19:22 +0300

    123

--- js/index.js
+++ js/index.js
@@ -4,3 +4,16 @@ const swiper = new Swiper('.reviews__swiper', {
     spaceBetween: 48,
     
 });
+
+// Получить элементы input и span
+const input = document.getElementById("calculation__input");
+const span = document.getElementById("my-span");
+
+// Добавить обработчик события "input" на input
+input.addEventListener("input", function() {
+  // Получить значение input и умножить его на 4
+  const value = input.value * 4;
+  
+  // Вывести результат в span
+  span.textContent = value;
+});
