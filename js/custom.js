4a6905bfde8ba0565139ffb93d77f4a0bda42ad7
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-06-03 14:56:08 +0300

    123

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+
+
+// nav menu 
+function openNav() {
+    document.getElementById("myNav").classList.toggle("menu_width");
+    document
+        .querySelector(".custom_menu-btn")
+        .classList.toggle("menu_btn-style");
+}
+
+// owl carousel script
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
