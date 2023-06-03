508e6f0b0da13a0941970faebcdef978d3670557
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-06-03 16:02:18 +0300

    123

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,39 @@ function openNav() {
     document
         .querySelector(".custom_menu-btn")
         .classList.toggle("menu_btn-style");
+  
 }
+
+let link1 = document.getElementById('nav-link1')
+let link2 = document.getElementById('nav-link2')
+let link3 = document.getElementById('nav-link3')
+let link4 = document.getElementById('nav-link4')
+    link3.addEventListener('click', function() {
+   
+        document.getElementById('myNav').classList.remove('menu_width')
+        document.querySelector(".custom_menu-btn").classList.remove("menu_btn-style");
+
+    })
+    link2.addEventListener('click', function() {
+   
+        document.getElementById('myNav').classList.remove('menu_width')
+        document.querySelector(".custom_menu-btn").classList.remove("menu_btn-style");
+
+    })
+    link1.addEventListener('click', function() {
+   
+        document.getElementById('myNav').classList.remove('menu_width')
+        document.querySelector(".custom_menu-btn").classList.remove("menu_btn-style");
+
+    })
+    link4.addEventListener('click', function() {
+   
+        document.getElementById('myNav').classList.remove('menu_width')
+        document.querySelector(".custom_menu-btn").classList.remove("menu_btn-style");
+
+    })
+
+   
 
 // owl carousel script
 $(".owl-carousel").owlCarousel({
