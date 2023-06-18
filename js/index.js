560d7d740bf05b17f88bc0d55ed4166b937653d4
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-06-18 17:42:42 +0300

    Первая версия

--- js/index.js
+++ js/index.js
@@ -1,35 +1,26 @@
-var swiper = new Swiper('.benefit__swiper', {  
-    slidesPerView: 'auto',
-    spaceBetween: 88,
-    speed: 2000,
-    loop: true,
-    autoplay: {
-      delay: 0,
-     
-    }
-  });
+document.addEventListener('DOMContentLoaded', () => {
 
-  var swiper2 = new Swiper(".offices__swiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-    },
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false
-      },
-
-  });
-
-  var swiper3 = new Swiper('.benefit__swiper1', {  
-    slidesPerView: 'auto',
-    spaceBetween: 30,
-    loop: false,
-  });
-
-  var swiper4 = new Swiper(".table__swiper", {
-    slidesPerView: "auto",
-    
-    });
-
+    const followCursor = () => { // объявляем функцию followCursor
+      const el = document.querySelector('.follow-cursor') // ищем элемент, который будет следовать за курсором
   
+      window.addEventListener('mousemove', e => { // при движении курсора
+        const target = e.target // определяем, где находится курсор
+        if (!target) return
+  
+        if (target.closest('a')) { // если курсор наведён на ссылку
+          el.classList.add('follow-cursor_active') // элементу добавляем активный класс
+        } else { // иначе
+          el.classList.remove('follow-cursor_active') // удаляем активный класс
+        }
+  
+        el.style.left = e.pageX + 'px' // задаём элементу позиционирование слева
+        el.style.top = e.pageY + 'px' // задаём элементу позиционирование сверху
+      })
+    }
+  
+    followCursor() // вызываем функцию followCursor
+
+
+    
+  
+  })
