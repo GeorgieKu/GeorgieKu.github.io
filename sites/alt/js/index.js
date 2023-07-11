df15a4924e122aeb2a80f2b8095f611e0cfe5f83
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-07-11 23:46:51 +0300

    Обнова луковки

---
+++ sites/alt/js/index.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.reviews__swiper', {
+    slidesPerView: 'auto',
+    loop: true,
+    spaceBetween: 48,
+    
+});
+
+// Получить элементы input и span
+const input = document.getElementById("calculation__input");
+const span = document.getElementById("my-span");
+
+// Добавить обработчик события "input" на input
+input.addEventListener("input", function() {
+  // Получить значение input и умножить его на 4
+  const value = input.value * 4;
+  
+  // Вывести результат в span
+  span.textContent = value;
+});
+
+new CircleType(document.getElementById('demo1'))
+.dir(-1)
+.radius(900);
