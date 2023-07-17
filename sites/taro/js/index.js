7cb9a712e0b9b1eb15f9564aa1b37d17baee9547
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-07-17 21:54:38 +0300

    newTaro

---
+++ sites/taro/js/index.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.iren__swiper', {
+
+    direction: 'horizontal',
+    loop: false,
+    
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+  });
+
+  const swiper2 = new Swiper('.iren__swiper2', {
+
+    direction: 'horizontal',
+    loop: false,
+    
+  
+
+  
+  });
