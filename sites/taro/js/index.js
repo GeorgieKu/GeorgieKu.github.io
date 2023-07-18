af23c9a8352f388fe814847325a67eda17f874aa
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-07-18 16:59:57 +0300

    123

--- sites/taro/js/index.js
+++ sites/taro/js/index.js
@@ -16,6 +16,15 @@ const swiper = new Swiper('.iren__swiper', {
     loop: false,
     
   
-
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   
+  });
+
+  var input = document.querySelector("#phone");
+  window.intlTelInput(input, {
+ 
+    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
   });
