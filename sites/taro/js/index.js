36128310b5203ca2d5545043678d06903cee7464
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-07-24 21:21:57 +0300

    123

--- sites/taro/js/index.js
+++ sites/taro/js/index.js
@@ -2,4 +2,8 @@ const swiper = new Swiper('.taro__swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: false,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
