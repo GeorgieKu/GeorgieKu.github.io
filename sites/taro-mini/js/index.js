4bd9f06a49b9084aa813cedd946aeb1617ec4fee
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-07-24 22:51:34 +0300

    123

---
+++ sites/taro-mini/js/index.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.taro__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
