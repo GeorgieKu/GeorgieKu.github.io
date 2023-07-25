75d6dfe29127b3d84f8152b512d5449460bbc1a0
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-07-25 16:03:36 +0300

    123

--- sites/taro-mini/js/index.js
+++ sites/taro-mini/js/index.js
@@ -1,9 +1,5 @@
-const swiper = new Swiper('.taro__swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: false,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+var input = document.querySelector("#phone");
+window.intlTelInput(input, {
+
+  utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
 });
