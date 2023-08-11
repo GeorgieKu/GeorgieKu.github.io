cf7bf8d0a35c606fbd63812a20a795c9176be34e
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-08-11 17:34:16 +0300

    new

--- sites/sync/js/index.js
+++ sites/sync/js/index.js
@@ -26,3 +26,36 @@ const swiper = new Swiper('.hero__swiper', {
   
 
   });
+
+  (function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })();
+
+  
+  const swiper3 = new Swiper('.advertising__swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    loop: false,
+    spaceBetween: 20,
+  
+  
+
+  });
+
