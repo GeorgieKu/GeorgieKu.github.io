29bb90f8ccc6abc729821a8fdb124efba547e767
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-08-16 10:52:20 +0300

    1new

---
+++ sites/lake/js/index.js
@@ -0,0 +1,50 @@
+new AirDatepicker('#datapicker', {
+    range: true,
+    multipleDatesSeparator: ' - '
+});
+
+const swiper = new Swiper('.hotel__swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 'auto',
+    initialSlide: 1,
+
+  });
+
+  const swiper2 = new Swiper('.entertainment__swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 110,
+    slidesPerView: 'auto',
+
+  });
+
+  const rooms = new Swiper('.rooms', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
+
+  });
+
+  const rooms2= new Swiper('.rooms-2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 'auto',
+
+  });
+
+  const rooms3 = new Swiper('.rooms-3', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 'auto',
+
+  });
