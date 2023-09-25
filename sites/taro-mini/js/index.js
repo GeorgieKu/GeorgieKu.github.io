718f8965858aac8d598eb9a67ae830ce835b1b33
GeorgieKu <108537893+GeorgieKu@users.noreply.github.com>
2023-09-25 14:44:00 +0300

    123

--- sites/taro-mini/js/index.js
+++ sites/taro-mini/js/index.js
@@ -6,30 +6,12 @@ const swiper = new Swiper('.review__swiper', {
   
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+      el: '.review__pagination',
+      clickable: true,
     },
 
   });
 
-  const accordionItems = document.querySelectorAll('.accordion-item');
-
-  accordionItems.forEach(item => {
-    const header = item.querySelector('.accordion-header');
-    const content = item.querySelector('.accordion-content');
-  
-    // Удаляем класс 'active' и устанавливаем максимальную высоту 0 при загрузке страницы
-    item.classList.remove('active');
-    content.style.maxHeight = '0';
-  
-    header.addEventListener('click', () => {
-      item.classList.toggle('active');
-      if (item.classList.contains('active')) {
-        content.style.maxHeight = content.scrollHeight + 'px';
-      } else {
-        content.style.maxHeight = '0';
-      }
-    });
-  });
 
   var input = document.querySelector("#phone");
 window.intlTelInput(input, {
