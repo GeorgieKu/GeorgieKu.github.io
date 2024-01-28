a8835645f31f73ec6b8fa8bc6c12405e0448962e
George <108537893+GeorgieKu@users.noreply.github.com>
2024-01-28 16:01:23 +0300

    123

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("burger").addEventListener("click", function() {
+    document.querySelector("header").classList.toggle("open")
+  })
+})
 
 class ItcAccordion {
     constructor(target, config) {
