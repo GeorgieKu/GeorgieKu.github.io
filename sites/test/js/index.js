cb3e2d7d029d72a49fa6179617115475a6e799fb
George <108537893+GeorgieKu@users.noreply.github.com>
2024-03-27 20:57:46 +0300

    123

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -1,57 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function() {
-        document.querySelector("header").classList.toggle("open")
-    })
+const swiper = new Swiper('.hero__swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 40,
+    slidesPerView: 'auto',
 
-    const swiper = new Swiper('.catalog__swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        slidesPerView: 'auto',
-        spaceBetween: 40,
+  });
 
-    
-        // Navigation arrows
-        navigation: {
-          nextEl: '.catalog-next',
-          prevEl: '.catalog-prev',
-        },
+  const middleSizeSwiper = new Swiper('.middle-size-swiper__swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 40,
+    slidesPerView: 'auto',
 
-      });
+  });
 
-      const swiper2 = new Swiper('.photo__swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        slidesPerView: 'auto',
-        spaceBetween: 40,
-        navigation: {
-            nextEl: '.photo-next',
-            prevEl: '.photo-prev',
-          },
-    
+  const doubleSizeSwiper = new Swiper('.double-size-swiper__swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 40,
+    slidesPerView: 'auto',
 
+  });
 
-      });
-      
-      let callMeLabel = document.getElementById('callMe__label-1')
-      let callMeCheckbox = document.getElementById('callMe__checkbox-1')
+  const doubleSizeSwiper2 = new Swiper('.double-size-swiper__swiper-2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 40,
+    slidesPerView: 'auto',
 
-      callMeLabel.addEventListener('click', function() {
-        callMeCheckbox.classList.toggle('checked')
-      })
-
-      let callMeLabel2 = document.getElementById('callMe__label-2')
-      let callMeCheckbox2 = document.getElementById('callMe__checkbox-2')
-
-      callMeLabel2.addEventListener('click', function() {
-        callMeCheckbox2.classList.toggle('checked')
-      })
-
-      let calculationLabel = document.getElementById('calculation__label')
-      let calculationCheckbox = document.getElementById('calculation__checkbox')
-
-      calculationLabel.addEventListener('click', function() {
-        calculationCheckbox.classList.toggle('checked')
-      })
-})
-
+  });
