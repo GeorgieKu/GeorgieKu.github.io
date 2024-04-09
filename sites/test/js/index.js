d6918295c9df487a0560c09a56ad962e63be4178
George <108537893+GeorgieKu@users.noreply.github.com>
2024-04-09 15:03:13 +0300

    123

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -1,9 +1,82 @@
-let footerBtnUp = document.querySelector('.footer__btn-up')
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function() {
+        document.querySelector("header").classList.toggle("open")
+    })
 
-footerBtnUp.addEventListener('click', function() {
-    document.documentElement.scrollTop = 0;
+    const swiper = new Swiper('.hero__swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        
+        // Navigation arrows
+        navigation: {
+          nextEl: '.hero-next',
+          prevEl: '.hero-prev',
+        },
+      
+      });
+
+      const swiper2 = new Swiper('.realized__swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        
+        // Navigation arrows
+        navigation: {
+          nextEl: '.realized__swiper-btn-next',
+          prevEl: '.realized__swiper-btn-prev',
+        },
+      
+      });
+
+      
+      const swiper3 = new Swiper('.licenses-swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        
+        // Navigation arrows
+        navigation: {
+          nextEl: '.licenses__swiper-btn-next',
+          prevEl: '.licenses__swiper-btn-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                autoHeight: true,
+            },
+            576: {
+                slidesPerView: 'auto',
+            },
+        }
+      
+      });
+
+      const swiper4 = new Swiper('.news-swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        
+        // Navigation arrows
+        navigation: {
+          nextEl: '.news__swiper-btn-next',
+          prevEl: '.news__swiper-btn-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                autoHeight: true,
+            },
+            576: {
+                slidesPerView: 'auto',
+            },
+        }
+      
+      });
 })
 
-document.getElementById("burger").addEventListener("click", function() {
-    document.querySelector("header").classList.toggle("open")
-})
+
