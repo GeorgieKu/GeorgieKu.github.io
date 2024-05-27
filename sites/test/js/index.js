e6cd96b07d1a901d9c0a69995be4a4697d461a26
George <108537893+GeorgieKu@users.noreply.github.com>
2024-05-27 17:56:31 +0300

    123

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -14,4 +14,24 @@ document.addEventListener("DOMContentLoaded", function() {
           el: '.swiper-scrollbar',
         },
       });
+
+      let overlay = document.getElementById('overlay');
+      let callbackBtn = document.getElementById('callbackBtn');
+      let modal = document.getElementById('modal');
+      let closeBtn = document.getElementById('close');
+      
+      callbackBtn.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        modal.style.display = 'block';
+        modal.style.animation = 'fadeIn .3s ease'
+      })
+
+      closeBtn.addEventListener('click', function() {
+        modal.style.animation = 'fadeOut .3s ease';
+        overlay.style.display = 'none';
+      
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 300);
+      })
 })
