679de03717abfcceaf19472ff042797186308621
George <108537893+GeorgieKu@users.noreply.github.com>
2024-06-29 15:02:16 +0300

    123

--- sites/test2/js/index.js
+++ sites/test2/js/index.js
@@ -1,143 +1,156 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  let overlay = document.getElementById('overlay');
+  let callbackBtn = document.getElementById('callBackBtn');
+  let callbackBtn2 = document.getElementById('callBackBtn2');
+  let modal = document.getElementById('modal');
+  let closeBtn = document.getElementById('close');
+  
+
+
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector("header").classList.toggle("open")
-        document.getElementById("headerNav").classList.toggle("open")
-        if (document.querySelector("header").classList.contains('open')) {
-            document.querySelector("body").style.overflow = 'hidden'
-        }
-        else {
-            document.querySelector("body").style.overflow = 'auto'
-        }
     })
 
-    const menuLink = document.querySelector('.menu__link_red');
-    const dropdown = document.querySelector('.menu__dropdown');
+    function openAcc(toggleButton, content, toggleArrow) {
+      toggleButton.addEventListener('click', function() {
+          content.classList.toggle('open');
+          toggleArrow.classList.toggle('rotate');
+          
+      });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+      let toggleButtons = document.querySelectorAll('.acc');
+      let contents = document.querySelectorAll('.content');
+      
+      toggleButtons.forEach((toggleButton, index) => {
+          let toggleArrow = toggleButton.querySelector('.about__squad');
+          openAcc(toggleButton, contents[index], toggleArrow);
+      });
+  });
 
-    menuLink.addEventListener('click', (event) => {
-        event.preventDefault();
-        dropdown.classList.toggle('show');
-    });
+    var swiper = new Swiper(".projects__swiper-2", {
+        direction: 'vertical',
+        spaceBetween: 10,
+        slidesPerView: 3,
+        freeMode: true,
+        watchSlidesProgress: true,
+        breakpoints: {
+          320: {
+            direction: 'horizontal',
+        },
+          576: {
+            direction: 'vertical',
+          }
+      }
+      });
+      var swiper2 = new Swiper(".projects__swiper-1", {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".swiper-button-next-1",
+          prevEl: ".swiper-button-prev-1",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
 
-    document.addEventListener('click', (event) => {
-        if (!menuLink.contains(event.target) && !dropdown.contains(event.target)) {
-            dropdown.classList.remove('show');
-        }
-    });
+      });
 
- 
+      var swiper3 = new Swiper(".projects__swiper-4", {
+        direction: 'vertical',
+        spaceBetween: 10,
+        slidesPerView: 3,
+        freeMode: true,
+        watchSlidesProgress: true,
+        breakpoints: {
+          320: {
+            direction: 'horizontal',
+        },
+          576: {
+            direction: 'vertical',
+          }
+      }
+      });
+      var swiper4 = new Swiper(".projects__swiper-3", {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".swiper-button-next-2",
+          prevEl: ".swiper-button-prev-2",
+        },
+        thumbs: {
+          swiper: swiper3,
+        },
+      });
 
+      var swiper5 = new Swiper(".projects__swiper-6", {
+        direction: 'vertical',
+        spaceBetween: 10,
+        slidesPerView: 3,
+        freeMode: true,
+        watchSlidesProgress: true,
+        breakpoints: {
+          320: {
+            direction: 'horizontal',
+        },
+          576: {
+            direction: 'vertical',
+          }
+      }
+      });
+      var swiper6 = new Swiper(".projects__swiper-5", {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".swiper-button-next-3",
+          prevEl: ".swiper-button-prev-3",
+        },
+        thumbs: {
+          swiper: swiper5,
+        },
+      });
 
+      let formCheck = document.getElementById('formCheck')
+      let checkbox = document.getElementById('checkBox')
+  
+      formCheck.addEventListener('click', function() {
+          checkbox.classList.toggle('checked')
+      });
 
+      let formCheck2 = document.getElementById('formCheck2')
+      let checkbox2 = document.getElementById('checkBox2')
+  
+      formCheck2.addEventListener('click', function() {
+          checkbox2.classList.toggle('checked')
+      });
 
-    const dropdownButtons = document.querySelectorAll('.dropdown-btn');
-    let activeDropdown = null;
-
-    dropdownButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const dropdownContent = this.nextElementSibling;
-
-            // Закрываем предыдущий открытый список, если он существует и не является текущим
-            if (activeDropdown && activeDropdown !== dropdownContent) {
-                activeDropdown.style.display = 'none';
-            }
-
-            // Переключение видимости текущего списка
-            if (dropdownContent.style.display === 'block') {
-                dropdownContent.style.display = 'none';
-                activeDropdown = null;
-            } else {
-                dropdownContent.style.display = 'block';
-                activeDropdown = dropdownContent;
-            }
-        });
-    });
-
-    // Закрываем выпадающий список при клике вне его
-    window.addEventListener('click', function(event) {
-        if (!event.target.matches('.dropdown-btn')) {
-            if (activeDropdown) {
-                activeDropdown.style.display = 'none';
-                activeDropdown = null;
-            }
-        }
-    });
-
-
-        
+      let formCheck3 = document.getElementById('formCheck3')
+      let checkbox3 = document.getElementById('checkBox3')
+  
+      formCheck3.addEventListener('click', function() {
+          checkbox3.classList.toggle('checked')
+      });
 
     
+      callbackBtn.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        modal.style.display = 'block';
+        modal.style.animation = 'fadeIn .3s ease'
+      })
+
+      callbackBtn2.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        modal.style.display = 'block';
+        modal.style.animation = 'fadeIn .3s ease'
+      })
+
+      closeBtn.addEventListener('click', function() {
+        modal.style.animation = 'fadeOut .3s ease';
+        overlay.style.display = 'none';
+      
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 300);
+      })
+
 
 })
-
-function openTab(event, tabName) {
-    // Получаем все элементы с классом tab-content и скрываем их
-    const tabContents = document.querySelectorAll('.tab-content');
-    tabContents.forEach(tabContent => {
-        tabContent.style.display = 'none';
-    });
-
-    // Получаем все элементы с классом tab-btn и убираем класс active
-    const tabButtons = document.querySelectorAll('.tab-btn');
-    tabButtons.forEach(tabButton => {
-        tabButton.classList.remove('active');
-    });
-
-    // Показываем текущий таб и добавляем класс active к кнопке, которая открыла этот таб
-    document.getElementById(tabName).style.display = 'block';
-    event.currentTarget.classList.add('active');
-}
-
-const swiper = new Swiper('.hero__swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    spaceBetween: 20,
-  
-    // If we need pagination
-    pagination: {
-      el: '.hero__pagination',
-    },
-  
-  });
-
-  var swiper2 = new Swiper(".product__swiper", {
-    spaceBetween: 10,
-    slidesPerView: 'auto',
-    spaceBetween: 20,
-    centerdSlides: true,
-    thumbs: {
-      swiper: swiper3,
-    },
-    breakpoints: {
-        300: {
-            slidesPerView: 1,
-        },
-    
-    }
-  });
-
-var swiper3 = new Swiper(".product__swiper-2", {
-    spaceBetween: 30,
-    slidesPerView: 3,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-
-  var swiper3 = new Swiper(".accessories__swiper", {
-    spaceBetween: 10,
-    slidesPerView: 1,
-    pagination: {
-        el: '.accessories__pagination'
-    },
-  });
-
-  var swiper4 = new Swiper(".watched__swiper", {
-    spaceBetween: 10,
-    slidesPerView: 1,
-    pagination: {
-        el: '.watched__pagination'
-    },
-  });
-
-  new SimpleBar(document.getElementById('myElement'));
