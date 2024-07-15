0eded45169225f67b1553c9b37b3f1081d78fb87
George <108537893+GeorgieKu@users.noreply.github.com>
2024-07-16 00:17:36 +0300

    123

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -1,41 +1,116 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function() {
-        document.querySelector("header").classList.toggle("open")
-    })
-    let formCheck = document.getElementById('formCheck')
-    let checkbox = document.getElementById('checkBox')
+let hero = document.querySelector('.hero')
+let loader = document.querySelector('.loader')
+let loaderBtn = document.getElementById('loaderBtn')
+let loaderProcent = document.getElementById('loaderProcent')
+let miner = document.querySelector('.miner')
+let minerBtn = document.getElementById('minerBtn')
+let minerImg = document.getElementById('minerImg')
+let valuePerDay = document.getElementById('valuePerDay')
+let overlay = document.getElementById('overlay');
+let modal = document.getElementById('modal')
+let modalBtn = document.getElementById('modalBtn')
+let modal2 = document.getElementById('modal2')
+let earnValue = document.getElementById('earnValue')
+let modalBtn2 = document.getElementById('modalBtn2')
 
-    formCheck.addEventListener('click', function() {
-        checkbox.classList.toggle('checked')
-    });
+const animation = lottie.loadAnimation({
+    container: document.getElementById('animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: false, 
+    path: 'data.json' 
+});
+function start() {
+    loader.style.display = 'block';
+    loader.style.animation = 'slideFromBottom .5s ease';
+    animation.play();
+    
+    let percent = 0;
+    const increment = 100 / 60; // 100% за 6000 миллисекунд (6 секунд)
+    
+    const interval = setInterval(() => {
+        percent += increment;
+        percent = Math.min(percent, 100); // Ограничение до 100%
+        loaderProcent.textContent = `${Math.round(percent)}%`;
+        
+        if (percent >= 100) {
+            clearInterval(interval); // Остановка интервала
+            loaderProcent.style.display = 'none';
+            hero.style.display = 'none';
+            document.querySelector('.loader__text').textContent = 'Ваше устройство идеально подходит!';
+            loaderBtn.style.display = 'block';
+            animation.stop();
+        }
+    }, 100); // Обновление каждые 100 миллисекунд (или другой интервал, который вам нужен)
+}
+loaderBtn.addEventListener('click', function() {
+    loader.style.animation = 'slideToBottom .5s ease'
+        miner.style.display = 'block'
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 500);
 
-    const swiper = new Swiper('.rent__swiper', {
 
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.rent__btn-next',
-          prevEl: '.rent__btn-prev',
-        },
-      });
+})
 
-      let overlay = document.getElementById('overlay');
-      let callbackBtn = document.getElementById('callbackBtn');
-      let modal = document.getElementById('modal');
-      let closeBtn = document.getElementById('close');
-      
-      callbackBtn.addEventListener('click', function() {
+const animation2 = lottie.loadAnimation({
+    container: document.getElementById('animation2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true, 
+    path: 'data2.json' 
+});
+
+
+let value = 0.000000024;
+let intervalId;
+
+function updateValue() {
+    value += 0.000000024;
+    valuePerDay.textContent = value.toFixed(9);
+}
+
+minerBtn.addEventListener('click', function() {
+
+    updateValue();
+    intervalId = setInterval(updateValue, 1000);
+    minerImg.style.animation = 'rotate 2s ease infinite'
+    setTimeout(() => {
+        clearInterval(intervalId);
         overlay.style.display = 'block';
         modal.style.display = 'block';
-        modal.style.animation = 'fadeIn .3s ease'
-      })
+    }, 4000);
+});
 
-      closeBtn.addEventListener('click', function() {
-        modal.style.animation = 'fadeOut .3s ease';
-        overlay.style.display = 'none';
-      
-        setTimeout(() => {
-            modal.style.display = 'none';
-        }, 300);
-      })
-})
+function updateValue2() {
+    value += 0.000000424;
+    valuePerDay.textContent = value.toFixed(9);
+}
+
+modalBtn.addEventListener('click', function() {
+    overlay.style.display = 'none';
+    modal.style.display = 'none';
+    
+    updateValue2();
+    intervalId = setInterval(updateValue2, 1000);
+    setTimeout(() => {
+        clearInterval(intervalId);
+        earnValue.textContent = valuePerDay.textContent;
+        overlay.style.display = 'block';
+        modal2.style.display = 'block';
+    }, 4000);
+});
+
+modalBtn2.addEventListener('click', function() {
+    overlay.style.display = 'none';
+    modal.style.display = 'none';
+    
+    updateValue();
+    intervalId = setInterval(updateValue, 1000);
+    setTimeout(() => {
+        clearInterval(intervalId);
+        earnValue.textContent = valuePerDay.textContent;
+        overlay.style.display = 'block';
+        modal2.style.display = 'block';
+    }, 4000);
+});
