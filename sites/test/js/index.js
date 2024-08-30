3990ddb0d709022ccded8182bdc448dae9261d78
George <108537893+GeorgieKu@users.noreply.github.com>
2024-08-30 19:54:55 +0300

    123

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -2,4 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector("header").classList.toggle("open")
     })
+
+    const dateInputs = document.querySelectorAll('.date__input');
+    
+    dateInputs.forEach(function(dateInput) {
+     
+        dateInput.setAttribute('type', 'text');
+        
+   
+        new AirDatepicker(dateInput, {
+            dateFormat: 'dd.MM.yyyy',
+      
+            onSelect: function(formattedDate) {
+
+            }
+        });
+    });
+
 })
+
