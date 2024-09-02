bde73ef2c66d65f67adf5e3022c21d0980e35327
George <108537893+GeorgieKu@users.noreply.github.com>
2024-09-02 16:02:23 +0300

    123

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -3,21 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector("header").classList.toggle("open")
     })
 
-    const dateInputs = document.querySelectorAll('.date__input');
-    
-    dateInputs.forEach(function(dateInput) {
-     
-        dateInput.setAttribute('type', 'text');
-        
-   
-        new AirDatepicker(dateInput, {
-            dateFormat: 'dd.MM.yyyy',
-      
-            onSelect: function(formattedDate) {
 
-            }
-        });
-    });
 
 })
 
