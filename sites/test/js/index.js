3beed85bc324fdd0548dcd8172e133e1d974533e
George <108537893+GeorgieKu@users.noreply.github.com>
2024-09-05 00:53:08 +0300

    123

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -3,7 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector("header").classList.toggle("open")
     })
 
+    let statementPaidHiddenBlock = document.querySelector('.statement-paid__hidden-block');
+    let statementPaidBtn = document.querySelector('.statement-paid__btn');
+    let form = document.querySelector('.statement-paid__from');
 
+    form.addEventListener('submit', function(event) {
+
+        if (form.checkValidity()) {
+            event.preventDefault();
+            statementPaidHiddenBlock.style.display = 'flex'; 
+        }
+    });
 
 })
 
