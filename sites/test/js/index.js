e0efe797539451b78b3f7b7968e826dc0e162f7b
George <108537893+GeorgieKu@users.noreply.github.com>
2024-09-05 00:54:09 +0300

    213

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -3,17 +3,20 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector("header").classList.toggle("open")
     })
 
+
     let statementPaidHiddenBlock = document.querySelector('.statement-paid__hidden-block');
-    let statementPaidBtn = document.querySelector('.statement-paid__btn');
     let form = document.querySelector('.statement-paid__from');
 
-    form.addEventListener('submit', function(event) {
+    if(form) {
+        form.addEventListener('submit', function(event) {
 
-        if (form.checkValidity()) {
-            event.preventDefault();
-            statementPaidHiddenBlock.style.display = 'flex'; 
-        }
-    });
+            if (form.checkValidity()) {
+                event.preventDefault();
+                statementPaidHiddenBlock.style.display = 'flex'; 
+            }
+        });
+    }
+    
 
 })
 
