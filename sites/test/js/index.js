121290d3aa4a028afb117f8ce39ca01a9408b0ef
George <108537893+GeorgieKu@users.noreply.github.com>
2024-09-10 14:19:05 +0300

    123

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -1,56 +1,76 @@
-document.querySelectorAll('.custom-checkbox').forEach(checkbox => {
-    checkbox.addEventListener('click', function (event) {
-        event.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
 
-        const hiddenInput = this.querySelector('.hidden-checkbox');
-        const checkboxElement = this.querySelector('.checkbox');
+    //Аккардеон
+    let toggleButtons = document.querySelectorAll('.acc');
+    let contents = document.querySelectorAll('.content');
 
-        // Тоглим состояние чекбокса
-        if (hiddenInput.checked) {
-            hiddenInput.checked = false;
-            checkboxElement.classList.remove('active');
-        } else {
-            hiddenInput.checked = true;
-            checkboxElement.classList.add('active');
-        }
+    toggleButtons.forEach((toggleButton, index) => {
+        let toggleArrow = toggleButton.querySelector('button');
+        openAcc(toggleButton, contents[index], toggleArrow);
     });
-});
 
-// Обновляем значение и стили для первого слайдера
-const sliderEl2 = document.querySelector("#range2");
-const sliderValue2 = document.querySelector(".value2");
+    function openAcc(toggleButton, content, toggleArrow) {
+        toggleButton.addEventListener('click', function () {
+            content.classList.toggle('open');
+            toggleArrow.classList.toggle('rotate');
 
-const updateSliderValue2 = (value) => {
-  sliderValue2.textContent = value.toLocaleString('de-DE'); // Форматирование с разделителями
-};
+        });
+    }
+    
+    //Чекбоксы
+    document.querySelectorAll('.custom-checkbox').forEach(checkbox => {
+        checkbox.addEventListener('click', function (event) {
+            event.preventDefault();
 
-updateSliderValue2(Number(sliderEl2.value));
+            const hiddenInput = this.querySelector('.hidden-checkbox');
+            const checkboxElement = this.querySelector('.checkbox');
 
-sliderEl2.addEventListener("input", (event) => {
-  const tempSliderValue = Number(event.target.value); 
-  updateSliderValue2(tempSliderValue);
+            if (hiddenInput.checked) {
+                hiddenInput.checked = false;
+                checkboxElement.classList.remove('active');
+            } else {
+                hiddenInput.checked = true;
+                checkboxElement.classList.add('active');
+            }
+        });
+    });
 
-  const progress = (tempSliderValue / sliderEl2.max) * 100;
+    //Слайдеры
+    const sliderEl2 = document.querySelector("#range2");
+    const sliderValue2 = document.querySelector(".value2");
 
-  sliderEl2.style.background = `linear-gradient(to right, rgba(39, 191, 201, 1) ${progress}%, rgba(229, 231, 236, 1) ${progress}%)`;
-});
+    const updateSliderValue2 = (value) => {
+        sliderValue2.textContent = value.toLocaleString('de-DE');
+    };
 
-// Обновляем значение и стили для второго слайдера
-const sliderEl3 = document.querySelector("#range3");
-const sliderValue3 = document.querySelector(".value3");
+    updateSliderValue2(Number(sliderEl2.value));
 
-const updateSliderValue3 = (value) => {
-  const months = value; // Преобразуем количество лет в месяцы
-  sliderValue3.textContent = `${months} Months`; // Обновляем текстовое значение
-};
+    sliderEl2.addEventListener("input", (event) => {
+        const tempSliderValue = Number(event.target.value);
+        updateSliderValue2(tempSliderValue);
 
-updateSliderValue3(Number(sliderEl3.value));
+        const progress = (tempSliderValue / sliderEl2.max) * 100;
 
-sliderEl3.addEventListener("input", (event) => {
-  const tempSliderValue = Number(event.target.value); 
-  updateSliderValue3(tempSliderValue);
+        sliderEl2.style.background = `linear-gradient(to right, rgba(39, 191, 201, 1) ${progress}%, rgba(229, 231, 236, 1) ${progress}%)`;
+    });
 
-  const progress = (tempSliderValue / sliderEl3.max) * 100;
 
-  sliderEl3.style.background = `linear-gradient(to right, rgba(39, 191, 201, 1) ${progress}%, rgba(229, 231, 236, 1) ${progress}%)`;
+    const sliderEl3 = document.querySelector("#range3");
+    const sliderValue3 = document.querySelector(".value3");
+
+    const updateSliderValue3 = (value) => {
+        const months = value;
+        sliderValue3.textContent = `${months} Months`;
+    };
+
+    updateSliderValue3(Number(sliderEl3.value));
+
+    sliderEl3.addEventListener("input", (event) => {
+        const tempSliderValue = Number(event.target.value);
+        updateSliderValue3(tempSliderValue);
+
+        const progress = (tempSliderValue / sliderEl3.max) * 100;
+
+        sliderEl3.style.background = `linear-gradient(to right, rgba(39, 191, 201, 1) ${progress}%, rgba(229, 231, 236, 1) ${progress}%)`;
+    });
 });
