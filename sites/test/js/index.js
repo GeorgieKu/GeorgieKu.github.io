8c4705221f2659808508faf1a0c6ac6177329c9b
George <108537893+GeorgieKu@users.noreply.github.com>
2024-12-04 17:21:28 +0300

    123

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("burger").addEventListener("click", function () {
         document.querySelector("header").classList.toggle("open")
     })
 
@@ -7,49 +7,61 @@ document.addEventListener("DOMContentLoaded", function() {
     let headerCatalogList = document.querySelector('.header__catalog-list');
     let headerCatalogBlock = document.querySelector('.header__catalog-block')
 
-    headerCatalogBtn.addEventListener('click', function() {
+    headerCatalogBtn.addEventListener('click', function () {
         headerCatalogBlock.classList.toggle('open');
     })
 
     let requestCheckboxBlocks = document.querySelectorAll('.request__checkbox-block');
 
-requestCheckboxBlocks.forEach(function(requestCheckboxBlock) {
-    requestCheckboxBlock.addEventListener('click', function() {
-        let requestCheckbox = requestCheckboxBlock.querySelector('.request__checkbox');
-        requestCheckbox.toggleAttribute('checked');
-        let requestCustomCheckbox = requestCheckboxBlock.querySelector('.request__custom-checkbox');
-        requestCustomCheckbox.classList.toggle('active');
+    requestCheckboxBlocks.forEach(function (requestCheckboxBlock) {
+        requestCheckboxBlock.addEventListener('click', function () {
+            let requestCheckbox = requestCheckboxBlock.querySelector('.request__checkbox');
+            requestCheckbox.toggleAttribute('checked');
+            let requestCustomCheckbox = requestCheckboxBlock.querySelector('.request__custom-checkbox');
+            requestCustomCheckbox.classList.toggle('active');
+        })
     })
-})
 
 
-const swiper = new Swiper('.about__swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: false,
-    spaceBetween: 20,
-    slidesPerView: 1,
-    navigation: {
-        nextEl: '.about-next',
-        prevEl: '.about-prev',
-      },
-  });
+    const swiper = new Swiper('.about__swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        spaceBetween: 20,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.about-next',
+            prevEl: '.about-prev',
+        },
+    });
 
-  const swiper2 = new Swiper('.company__swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: false,
-    spaceBetween: 24,
-    slidesPerView: 'auto',
-    navigation: {
-        nextEl: '.company-next',
-        prevEl: '.company-prev',
-      },
-    pagination: {
-        el: ".company__pagination"
-    }
-    
-  });
+    const swiper2 = new Swiper('.company__swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        spaceBetween: 24,
+        slidesPerView: 'auto',
+        navigation: {
+            nextEl: '.company-next',
+            prevEl: '.company-prev',
+        },
+        pagination: {
+            el: ".company__pagination"
+        }
+
+    });
+
+    const swiper3 = new Swiper('.info__swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        spaceBetween: 20,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.info-next',
+            prevEl: '.info-prev',
+        },
+    });
 })
 
 function openModal() {
@@ -58,7 +70,13 @@ function openModal() {
     overlay.style.display = 'block';
     modal.setAttribute('open', 'open');
 
-    overlay.addEventListener('click', function() {
+    overlay.addEventListener('click', function () {
+        modal.removeAttribute('open', 'open');
+        overlay.style.display = 'none';
+    })
+    let closeModal = document.getElementById('modalClose');
+
+    closeModal.addEventListener('click', function () {
         modal.removeAttribute('open', 'open');
         overlay.style.display = 'none';
     })
