91accecb77313cfcdfc0509c281bdff2c7c4fa02
George <108537893+GeorgieKu@users.noreply.github.com>
2025-01-28 17:16:14 +0300

    123

--- sites/test/js/index.js
+++ sites/test/js/index.js
@@ -6,11 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let showMoreBtn = document.getElementById('showMore');
     let hiddenList = document.querySelector('.hidden-576')
+    if(showMoreBtn) {
+        showMoreBtn.addEventListener('click', function() {
+            hiddenList.classList.toggle('hidden-576');
+            showMoreBtn.classList.toggle('open')
+        })
+    }
 
-    showMoreBtn.addEventListener('click', function() {
-        hiddenList.classList.toggle('hidden-576');
-        showMoreBtn.classList.toggle('open')
-    })
 
     let terminalsBtns = document.querySelectorAll('.terminals__btns button');
 
@@ -37,14 +39,36 @@ function openModal() {
     modal.setAttribute('open', 'open');
     modal.style.animation = 'sacler .25s ease'
 }
+if (closeModal) {
+    closeModal.addEventListener('click', function() {
+        allAboutModal.classList.remove('active');
+        modal.removeAttribute('open', 'open');
+    })
+}
 
-closeModal.addEventListener('click', function() {
-    allAboutModal.classList.remove('active');
-    modal.removeAttribute('open', 'open');
-})
+if(overlay) {
+    overlay.addEventListener('click', function() {
+        allAboutModal.classList.remove('active');
+        modal.removeAttribute('open', 'open');
+    })
+}
 
-overlay.addEventListener('click', function() {
-    allAboutModal.classList.remove('active');
-    modal.removeAttribute('open', 'open');
-})
 
+
+function openAcc(toggleButton, content, toggleArrow) {
+    toggleButton.addEventListener('click', function() {
+        content.classList.toggle('open');
+        toggleArrow.classList.toggle('rotate');
+        
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let toggleButtons = document.querySelectorAll('.acc');
+    let contents = document.querySelectorAll('.content');
+    
+    toggleButtons.forEach((toggleButton, index) => {
+        let toggleArrow = toggleButton.querySelector('svg');
+        openAcc(toggleButton, contents[index], toggleArrow);
+    });
+});
