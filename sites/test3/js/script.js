ef6ac34fa3d3d0ba4e3bade0f0a6d8c65ddf96af
George <108537893+GeorgieKu@users.noreply.github.com>
2025-10-20 22:21:04 +0300

    update

--- sites/test3/js/script.js
+++ sites/test3/js/script.js
@@ -7,12 +7,35 @@ contactChecks.forEach(function (contactCheck) {
         contactCheck.classList.toggle('active')
     })
 })
-let modal = document.getElementById('modal');
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([.\^$*+?()[\]{}|])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
 
-modal.showModal()
 
-let acceptBtn = document.getElementById('acceptBtn');
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (encodeURIComponent(value) || "") + expires + "; path=/";
+}
 
+const modal = document.getElementById('modal');
+const acceptBtn = document.getElementById('acceptBtn');
+
+window.addEventListener('load', function () {
+    if (!getCookie('modalAccepted')) {
+        modal.showModal();
+    }
+});
+
+// Обработчик клика по кнопке accept
 acceptBtn.addEventListener('click', function () {
-    modal.close()
-})
+    setCookie('modalAccepted', 'true', 30);
+    modal.close();
+});
