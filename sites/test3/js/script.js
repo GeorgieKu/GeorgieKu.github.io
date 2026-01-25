3e66561308452df3f6b568ac88f007e835c7b79a
George <108537893+GeorgieKu@users.noreply.github.com>
2026-01-25 17:47:58 +0300

    update

--- sites/test3/js/script.js
+++ sites/test3/js/script.js
@@ -1,10 +1,9 @@
 let heroSwiper = document.querySelector('.hero__swiper')
 if (heroSwiper) {
     const heroSwiper = new Swiper('.hero__swiper', {
-        // Optional parameters
+
         direction: 'horizontal',
 
-        // If we need pagination
         pagination: {
             el: '.hero__pagination',
         },
@@ -14,7 +13,7 @@ if (heroSwiper) {
 let prdouctionSwiper = document.querySelector('.production__swiper')
 if (prdouctionSwiper) {
     const prdouctionSwiper = new Swiper('.production__swiper', {
-        // Optional parameters
+
         direction: 'horizontal',
         slidesPerView: "auto",
         spaceBetween: 20,
@@ -38,7 +37,7 @@ if (prdouctionSwiper) {
 let reviewsSwiper = document.querySelector('.reviews__swiper')
 if (reviewsSwiper) {
     const reviewsSwiper = new Swiper('.reviews__swiper', {
-        // Optional parameters
+
         direction: 'horizontal',
         slidesPerView: "auto",
         spaceBetween: 20,
@@ -61,7 +60,7 @@ if (reviewsSwiper) {
 let newsSwiper = document.querySelector('.reviews__swiper')
 if (newsSwiper) {
     const newsSwiper = new Swiper('.news__swiper', {
-        // Optional parameters
+
         direction: 'horizontal',
         slidesPerView: "auto",
         spaceBetween: 20,
@@ -199,6 +198,169 @@ if (sort) {
             sortBtn.textContent = sortBtnText;
         })
     })
+}
+
+
+
+let videoCards = document.querySelectorAll('.video-gallery__card')
+
+if (videoCards) {
+    videoCards.forEach(function (videoCard) {
+        let video = videoCard.querySelector('.video-gallery__video');
+        let videoPlayBtn = videoCard.querySelector('.video-gallery__btn');
+        videoPlayBtn.addEventListener('click', function () {
+            if (videoPlayBtn.classList.contains('played')) {
+                video.load()
+                videoPlayBtn.classList.remove('played')
+            } else {
+                videoPlayBtn.classList.add('played')
+                video.play()
+            }
+
+        })
+
+        video.addEventListener("ended", function () {
+            this.src = this.src
+        });
+    })
+}
+
+let videoSwiper = document.querySelector('.video-gallery__swiper')
+if (videoSwiper) {
+    const videoSwiper = new Swiper('.video-gallery__swiper', {
+        direction: 'horizontal',
+        slidesPerView: "auto",
+        spaceBetween: 20,
+        breakpoints: {
+            300: {
+                spaceBetween: 10,
+            },
+            576: {
+                spaceBetween: 20,
+            }
+        }
+
+    });
+}
+
+
+let contactsPageSwiper = document.querySelector('.contacts-page__swiper')
+if (contactsPageSwiper) {
+    const contactsPageSwiper = new Swiper('.contacts-page__swiper', {
+        direction: 'horizontal',
+        slidesPerView: "auto",
+        spaceBetween: 20,
+    });
+}
+//  карта на странице контактов в дестктопе
+const popup = document.getElementById('popup');
+
+if (popup && innerWidth > 993) {
+    function showPopup(evt, text) {
+        popup.textContent = text;
+        popup.style.left = (evt.pageX + 10) + 'px';
+        popup.style.top = (evt.pageY + 10) + 'px';
+        popup.classList.add('visible');
+    }
+
+    function hidePopup() {
+        popup.classList.remove('visible');
+    }
+}
+
+
+
+
+// Список и карта на странице контактов в мобилке
+const paths = document.querySelectorAll('path.show-popup');
+if (paths) {
+    paths.forEach(path => {
+        path.addEventListener('mouseenter', evt => {
+            const text = path.getAttribute('data-popup') || 'Нет описания';
+            showPopup(evt, text);
+        });
+        path.addEventListener('mousemove', evt => {
+            popup.style.left = (evt.pageX + 10) + 'px';
+            popup.style.top = (evt.pageY + 10) + 'px';
+        });
+        path.addEventListener('mouseleave', hidePopup);
+    });
+
+    (function () {
+        const customSelect = document.querySelector('.custom-select');
+        const selected = customSelect.querySelector('.select-selected');
+        const itemsContainer = customSelect.querySelector('.select-items');
+        const options = itemsContainer.querySelectorAll('.select-item');
+        const svgPaths = document.querySelectorAll('svg path[data-value]');
+
+        function openDropdown() {
+            itemsContainer.classList.add('open');
+            selected.classList.add('open');
+            customSelect.setAttribute('aria-expanded', 'true');
+        }
+
+        function closeDropdown() {
+            itemsContainer.classList.remove('open');
+            selected.classList.remove('open');
+            customSelect.setAttribute('aria-expanded', 'false');
+        }
+
+        function toggleDropdown() {
+            if (itemsContainer.classList.contains('open')) {
+                closeDropdown();
+            } else {
+                openDropdown();
+            }
+        }
+
+        function clearHighlight() {
+            svgPaths.forEach(path => path.classList.remove('highlighted'));
+        }
+
+        function highlightPath(value) {
+            clearHighlight();
+            const pathToHighlight = document.querySelector(`svg path[data-value="${value}"]`);
+            if (pathToHighlight) {
+                pathToHighlight.classList.add('highlighted');
+            }
+        }
+
+        function selectOption(option) {
+            options.forEach(opt => opt.classList.remove('selected'));
+            option.classList.add('selected');
+
+            selected.textContent = option.textContent;
+            selected.dataset.value = option.dataset.value;
+
+            highlightPath(option.dataset.value);
+        }
+
+
+        let initialSelected = itemsContainer.querySelector('.select-item.selected');
+        if (initialSelected) {
+            highlightPath(initialSelected.dataset.value);
+        }
+
+        selected.addEventListener('click', () => {
+            toggleDropdown();
+        });
+
+        options.forEach(option => {
+            option.addEventListener('click', () => {
+                selectOption(option);
+                closeDropdown();
+            });
+        });
+
+
+        document.addEventListener('click', e => {
+            if (!customSelect.contains(e.target)) {
+                closeDropdown();
+            }
+        });
+
+
+    })();
 }
     document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("burger").addEventListener("click", function () {
